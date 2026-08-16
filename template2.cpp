e6cdfa930ee7cd0c8d70d02a7c0559f09512130c
bela <belabelial666@aol.com>
2026-08-16 00:13:18 -0300

    feat: secondary template added

---
+++ template2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define no_sync ios_base::sync_with_stdio(false);
+#define optimize_cin_cout cin.tie(nullptr);
+#define endl "\n"
+
+int32_t main(){
+    no_sync;
+    optimize_cin_cout;
+
+    return 0;
+}
